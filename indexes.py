4b9f5f6c83f77d60a3b7ae999b47bc200cdc5cb5
William Sena Neves <matriculado@BON046.acad.fgv.br>
2018-09-28 15:10:45 -0300

    Escolha de indices parte 1

---
+++ indexes.py
@@ -0,0 +1,22 @@
+indexes = '''
+http://api.worldbank.org/v2/en/indicator/NY.GDP.PCAP.CD?downloadformat=excel pib per capita 
+http://api.worldbank.org/v2/en/indicator/NV.AGR.TOTL.ZS?downloadformat=excel Agricultura, valor adicionado
+http://api.worldbank.org/v2/en/indicator/AG.PRD.FOOD.XD?downloadformat=excel Índice de produção de alimentos 
+http://api.worldbank.org/v2/en/indicator/AG.LND.ARBL.HA.PC?downloadformat=excel Terra arável
+http://api.worldbank.org/v2/en/indicator/AG.SRF.TOTL.K2?downloadformat=excel Superfície 
+http://api.worldbank.org/v2/en/indicator/AG.LND.TRAC.ZS?downloadformat=excel Maquinaria agrícola
+http://api.worldbank.org/v2/en/indicator/EN.ATM.CO2E.PC?downloadformat=excel emissao de CO2
+http://api.worldbank.org/v2/en/indicator/SP.URB.TOTL.IN.ZS?downloadformat=excel População urbana
+http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=excel população total
+http://api.worldbank.org/v2/en/indicator/SP.POP.GROW?downloadformat=excel crescimento populacional 
+http://api.worldbank.org/v2/en/indicator/IC.BUS.EASE.XQ?downloadformat=excel facilidade para fazer negócios 
+http://api.worldbank.org/v2/en/indicator/EG.USE.PCAP.KG.OE?downloadformat=excel uso de energia
+http://api.worldbank.org/v2/en/indicator/TX.VAL.TECH.CD?downloadformat=excel exportação alta tecnologia 
+http://api.worldbank.org/v2/en/indicator/GB.XPD.RSDV.GD.ZS?downloadformat=excel despesas pesquisa e desenvolvimento
+http://api.worldbank.org/v2/en/indicator/ST.INT.XPND.MP.ZS?downloadformat=excel turismo importação
+http://api.worldbank.org/v2/en/indicator/NE.EXP.GNFS.ZS?downloadformat=excel exportações bense e serviços
+http://api.worldbank.org/v2/en/indicator/NE.IMP.GNFS.ZS?downloadformat=excel importação de bens e serviços
+http://api.worldbank.org/v2/en/indicator/TT.PRI.MRCH.XD.WD?downloadformat=excel Índice de términos de intercambio
+http://api.worldbank.org/v2/es/indicator/ST.INT.RCPT.XP.ZS?downloadformat=excel turismo exportação
+'''
+
