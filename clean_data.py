e599d3dcddd03294b6384eacd1fa23702fc59af5
Felvc027 <30601418+Felvc027@users.noreply.github.com>
2018-10-22 18:19:37 -0300

    Update clean_data.py
    Adding a auxiliar function longest_seq_data and updating clean_tb and placing the functions seaching_by as auxiliary to the main function data_search.

--- clean_data.py
+++ clean_data.py
@@ -2,6 +2,20 @@ import pandas as pd
 import numpy as np
 import functools as f
 
+def longest_seq_data(data):
+    max_s = []
+    for i in data.T:
+        index = data.T[i].isnull()
+        nans = data.T[index].index.values
+        fnans = np.concatenate((np.array([-1]),nans-1960))
+        lnans = np.concatenate((nans-1960,np.array([len(data.columns)-3])))
+        seq = lnans-fnans-1
+        if len(nans)==0:
+            max_s.append(len(data.columns)-3)
+        else:
+            max_s.append(max(seq))
+    return pd.Series(max_s,index=data.index)
+
 def clean_tb(df,crt,by_count=True):
     '''\nDocstring for clear_tb
     Parameters:
@@ -16,38 +30,39 @@ def clean_tb(df,crt,by_count=True):
     df = df.dropna(axis = 1,how = 'all')
     if by_count:
         df = df.dropna(thresh = crt)
-    
-        
+    else:
+        l_sequence = longest_seq_data(df)
+        df = df.drop(index=[country for country in l_sequence.index if l_sequence(country) < crt])
     df.index = list(df['Country Name'])
     df = df.drop(['Country Name'],axis=1)
-    return df            
-
-def search_by_year(list_tables,year):
-    l = []
-    labels = []
-    
-    for i in list_tables:
-        if year in i.columns:
-            l.append(pd.DataFrame(i[year]))
-            df = f.reduce(lambda a,b: a.join(b,how='outer',rsuffix=i['Indicator Code'][0]),l)
-            l = [df]
-            labels.append(i['Indicator Name'][0])
-    df.columns = labels
-    return df
-
-def search_by_country(list_tables,country):
-    l = []
-    labels = []
-    for i in list_tables:
-        if country in i.index:
-            l.append(pd.DataFrame(i.T[country]))
-            df = f.reduce(lambda a,b: a.join(b,how='outer',rsuffix=i['Indicator Code'][0]),l)
-            l = [df]
-            labels.append(i['Indicator Name'][0])
-    df.columns = labels
-    return df.drop(['Country Code','Indicator Name','Indicator Code'])
+    return df          
 
 def data_search(data,by,key):
+    def search_by_year(list_tables,year):
+        l = []
+        labels = []
+
+        for i in list_tables:
+            if year in i.columns:
+                l.append(pd.DataFrame(i[year]))
+                df = f.reduce(lambda a,b: a.join(b,how='outer',rsuffix=i['Indicator Code'][0]),l)
+                l = [df]
+                labels.append(i['Indicator Name'][0])
+        df.columns = labels
+        return df
+    
+    def search_by_country(list_tables,country):
+        l = []
+        labels = []
+        for i in list_tables:
+            if country in i.index:
+                l.append(pd.DataFrame(i.T[country]))
+                df = f.reduce(lambda a,b: a.join(b,how='outer',rsuffix=i['Indicator Code'][0]),l)
+                l = [df]
+                labels.append(i['Indicator Name'][0])
+        df.columns = labels
+        return df.drop(['Country Code','Indicator Name','Indicator Code'])
+
     if by == 'country':
         return search_by_country(data,key)
     elif by == 'year':
