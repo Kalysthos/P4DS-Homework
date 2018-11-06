3b6e19afbda7fbb6ade8a5b2db5fc897a12968a4
William Sena Neves <matriculado@BON046.acad.fgv.br>
2018-11-06 21:31:00 -0200

    a porra de um commit

--- data2.py
+++ data2.py
@@ -1,3 +1,4 @@
 import pandas as pd
+
 table_links = [i for i in open('Indicators.txt').read().split() if i.startswith('htt')]
-data = [pd.read_excel(i, Sheet_name='Data',header=3) for i in table_links]
+data2 = [pd.read_excel(i, Sheet_name='Data',header=3) for i in table_links]
