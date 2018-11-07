01042b0dd82d09de5a4883634d56a94146385636
William Sena Neves <matriculado@BON046.acad.fgv.br>
2018-11-07 11:05:20 -0200

    reparos

--- analyze.py
+++ analyze.py
@@ -1,19 +1,17 @@
-def analyze(data, parameter, nocountries=False, log=False):
+def analyze(data, parameter='GDP per capita (current US$)', nocountries=True):
     '''Dataframe of data from year (integer), index (name) or country (name with "c-" at start)'''
 
     import pandas as pd
     countries = [i.replace("\n", "") for i in open("countries.txt")]
     
     if type(parameter) == int:
-        country = pd.DataFrame(data[0][str(parameter)])
-        country.columns = [data[0]['Indicator Name'][0]]
+        country = pd.DataFrame(data[0][str(parameter)]).rename(columns=lambda x: data[0]['Indicator Name'][0])
         for k,i in enumerate(data[1:]):
             country = country.join(pd.Series(i[str(parameter)], name=data[k+1]["Indicator Name"][0]))
         return country.reindex(countries).dropna(how='all') if nocountries else country.dropna(how='all')
     
     elif parameter[0:2] == "c-":
-        ages = pd.DataFrame(data[0].T[parameter[2:]][4:])
-        ages.columns = [data[0]["Indicator Name"][0]]
+        ages = pd.DataFrame(data[0].T[parameter[2:]][4:]).rename(columns=lambda x: data[0]["Indicator Name"][0])
         for k,i in enumerate(data[1:]):
             ages = ages.join(pd.Series(i.T[parameter[2:]][4:], name=data[k+1]["Indicator Name"][0]))
         return ages.dropna(how='all')
@@ -21,7 +19,5 @@ def analyze(data, parameter, nocountries=False, log=False):
     else:
         for i in data:
             if i['Indicator Name'][0] == parameter:
-                saida = i.drop("Indicator Name", axis=1)
-                break
-        return saida.reindex(countries).dropna(how='all') if nocountries else saida.dropna(how='all')
+                return i.drop("Indicator Name", axis=1).reindex(countries).dropna(how='all') if nocountries else i.drop("Indicator Name", axis=1).dropna(how='all')
 
--- plots.py
+++ plots.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from analyze import analyze 
 from sklearn import linear_model
     
 def scatter(data, age, index1, index2):
-    '''Generates scatter plot and trend line in comparison between two indices in a given year''''
+    '''Generates scatter plot and trend line in comparison between two indices in a given year'''
 
     df = np.log(analyze(data, 2017)[[index1]+[index2]].dropna())
     y = np.array(df[index1]).reshape(-1, 1)
@@ -13,12 +14,12 @@ def scatter(data, age, index1, index2):
     plt.plot(x, model.predict(x), color='red', linewidth=2.)
     plt.scatter(y=y, x=x, color='blue', s=50, alpha=.5)
     plt.title('Tendency line log-log')
-    plt.xlabel(indice[0])
-    plt.ylabel(indice[1])
+    plt.xlabel(index1)
+    plt.ylabel(index2)
     plt.show()
     
 def trend(data, country, index, start, end):
-    '''Fenerates the trend line over the years (start and end) of the requested indices''''
+    '''Fenerates the trend line over the years (start and end) of the requested indices'''
 
     for i in country:
         df = analyze(data, "c-"+i, nocountries=True)[index].loc[[str(i) for i in range(start, end+1)]]
@@ -29,7 +30,7 @@ def trend(data, country, index, start, end):
     plt.ylabel(index)
     plt.show
     
-def lmh(datam, age, index, x):
+def lmh(data, age, index, x):
     '''Get the x lowest, middle and highest values'''
     
     df = analyze(data, age)[index].dropna().sort_values()
