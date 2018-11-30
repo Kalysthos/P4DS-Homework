6511fd4b3a1581becb4de7e473732c16c1d6abfa
William Sena Neves <matriculado@BON046.acad.fgv.br>
2018-11-30 15:41:46 -0200

    documentando plots no New

--- New/plots.py
+++ New/plots.py
@@ -3,30 +3,72 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import linear_model, preprocessing
     
-norm = lambda x: preprocessing.MinMaxScaler().fit_transform(np.array(x, dtype="float64").reshape(-1, 1))
+norm = lambda df: preprocessing.MinMaxScaler().fit_transform(np.array(df, dtype="float64").reshape(-1, 1))
     
-def trend(data, index, country, years):
-    '''Generates the trend over the years (list of ints) of the countries (list of strs) and requested index (str)'''
+def trend(data, index, countries, years):
+    '''
+    Generates a trend graph for a given index in a given period of time for any set of countries
     
-    for i in country:
-        plt.plot(data.loc[(index, i), [str(i) for i in range(years[0], years[1] + 1)]])
+    Parameters:
+    -----------
+    
+        data -> dataframe of data
+        index -> index string
+        countries -> list with indexes strings
+        years -> list with integers indicating the start and end year for the analysis
         
-    plt.legend(country)
+    Return:
+    -------
+        
+        matplotlib.pyplot.plot with the trend graph
+    
+    Example:
+    --------
+    
+        trend(data, 'GDP per capita (current US$)', ['Afghanistan', 'Brazil'], [2000, 2017]) 
+    '''
+    
+    for country in countries:
+        plt.plot(data.loc[(index, country), [str(year) for year in range(years[0], years[1] + 1)]])
+    
     plt.title('Tendency')
-    plt.setp(plt.xticks()[1], rotation=60)
+    plt.legend(countries)
     plt.xlabel('Years')
     plt.ylabel(index)
+    plt.setp(plt.xticks()[1], rotation=60)
     plt.show()
     
-def lmh(data, indexes, year, x):
-    '''Generates the x (int) smaller, middle and higher values for the indicated year (int) and index (str)'''
+def xsml(data, indexes, year, X):
+    '''
+    Generates a bar graph with the X smallest, the X medium, and the X largest values of an index or comparison between two indexes in a given year
+    
+    Parameters:
+    -----------
+    
+        data -> dataframe of data
+        indexes -> index string or list with indexes strings
+        year -> integer indicating the year for analysis
+        X -> integer indicating the number of countries for the analyze 
+        
+    Return:
+    -------
+        
+        matplotlib.pyplot.plot with the bar graph
+        
+    Examples:
+    ---------
+    
+        xsml(data,  'Economic freedom', 2017, 3)
+        
+        xsml(data,  ['Economic freedom', 'GDP per capita (current US$)'], 2017, 3)
+    '''
     
     if type(indexes) == str:
         df = data.loc[indexes].drop('World')[str(year)].dropna().sort_values()
         
-        amount = int(len(df)/2)-int(x/2)
-        df.iloc[list(range(0,x))+list(range(amount, amount+x))+list(range(-x,0))].plot(kind="bar")
-        plt.title('Bar plot')
+        amount = int(df.shape[0]/2)-int(X/2)
+        df.iloc[list(range(0,X))+list(range(amount, amount+X))+list(range(-X,0))].plot(kind="bar")
+        plt.title('Bar graph')
         plt.xlabel('Country')
         plt.ylabel(indexes)
         plt.show()
@@ -38,18 +80,43 @@ def lmh(data, indexes, year, x):
         df['0'] = norm(df['0'])
         df[str(year)] = norm(df[str(year)])
         
-        amount = int(df.shape[0]/2)-int(x/2)
-        bars = df.iloc[list(range(0,x))+list(range(amount, amount+x))+list(range(-x,0))]
-        bars.plot(kind='bar')
+        amount = int(df.shape[0]/2)-int(X/2)
+        df.iloc[list(range(0,X))+list(range(amount, amount+X))+list(range(-X,0))].plot(kind='bar')
+        plt.title('Bar graph')
         plt.legend(indexes)
-        plt.title('Bar plot')
-        plt.xlabel('Country')
+        plt.xlabel('Country') 
+        plt.ylabel('Normalized values')
         plt.show()
     
 def trend_years(data, indexes, country, years, plot=True):
-    '''Generates the trend line or the r squared, coefficient and number of countries (plot=False) of the years (list of ints) of the country (str) and the requested index (str)'''
+    '''
+    Trend graph for a given country and time period of any set of indexes or an index with its trend line. Or a tuple with the details of the trend line
     
-    df = data.loc[(indexes, country), [str(i) for i in range(years[0], years[1] + 1)]]
+    Parameters:
+    -----------
+    
+        data -> dataframe of data
+        indexes ->  indice string or list with indexes strings
+        country -> country string
+        years -> list with integers indicating the start and end year for the analysis
+        plot -> boolean indicating whether the return will be a graph (True) or a tuple with the details of the trend line (False and indexes as an index string)
+        
+    Return:
+    -------
+        
+        matplotlib.pyplot.plot with the trend graph or a tuple with R-Square, coefficient and number of countries in the index trend line analysis
+        
+    Examples:
+    ---------
+    
+        trend_years(data, ['Economic freedom', 'GDP per capita (current US$)'], 'World', [2000, 2017])
+        
+        trend_years(data, 'Economic freedom', 'World', [2000, 2017])
+        
+        trend_years(data, 'Economic freedom', 'World', [2000, 2017], False)
+    '''
+    
+    df = data.loc[(indexes, country), [str(year) for year in range(years[0], years[1] + 1)]]
     
     if type(indexes) == str:
         df = df.dropna()
@@ -60,20 +127,24 @@ def trend_years(data, indexes, country, years, plot=True):
         if plot:
             plt.plot(x, model.predict(x), color='red', linewidth=2.)
             plt.plot(x, y)
+            plt.title('Trend graph and trend line')
             plt.legend(["R**2 = {:0.4}".format(model.score(x, y)), "Coef = {:0.4}".format(model.coef_[0][0])])
-            plt.title('Tendency line')
             plt.setp(plt.xticks()[1], rotation=60)
-            plt.xlabel('Years')
-            plt.ylabel(indexes)
+            plt.xlabel('Normalized years')
+            plt.ylabel('Normalized values')
             plt.show()
 
         else:
-            return model.score(x, y), model.coef_[0][0], len(x)
+            return model.score(x, y), model.coef_[0][0], x.shape[0]
         
     else:
         df = df.dropna(1)
         for ind in indexes:
             plt.plot(df.columns, norm(df.loc[ind]))
+            
+        plt.title('Trend graphs')
         plt.legend(indexes)
         plt.setp(plt.xticks()[1], rotation=60)
+        plt.xlabel('Years')
+        plt.ylabel('Normalized values')
         plt.show() 
