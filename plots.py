98ae791ecf5909d1e9cd74eba4ed5b73cf66c84c
William Sena Neves <matriculado@BON046.acad.fgv.br>
2018-12-05 09:14:54 -0200

    arrumando repositório

--- plots.py
+++ plots.py
@@ -1,141 +1,161 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-from analyze import analyze 
 from sklearn import linear_model, preprocessing
-
-class divid:
-    def __init__(self, x, n):
-        self.range, self.cut = np.arange(x), [None]*n
-        for i in range(n):
-            self.cut[i] = x//n*i + np.arange(x//n + 1 if x%n - i > 0 else x//n)     
-    def split(self, n):
-        return np.setdiff1d(self.range, self.cut[n])
     
-def findlinear(x, y, div=10):
-    order = np.random.permutation(x.shape[0])
-    x, y = x[order], y[order]
-    split = divid(x.shape[0], div)
-    r2 = 0
-    for i in range(div):
-        index = split.split(i)
-        xtest, ytest = x[index], y[index]
-        modeltest = linear_model.LinearRegression().fit(xtest, ytest)
-        r2test = modeltest.score(xtest,ytest)
-        if r2test > r2:
-            r2, model = r2test, modeltest
-    return model
-
-def remove_outliers(x, y, percent=2.5):
-    model = findlinear(x, y)
-    dist = abs(y - model.predict(x))
-    desv = abs(dist - dist.mean())/np.std(dist)
-    part = (100-percent)/100*x.shape[0]
-    for m in np.arange(2., 5., 0.25):
-        std = desv < m 
-        if std.sum() > part:
-            return x[std].reshape(-1, 1), y[std].reshape(-1, 1)
+norm = lambda df: preprocessing.MinMaxScaler().fit_transform(np.array(df, dtype="float64").reshape(-1, 1))
     
-def scatter(data, year, indexes, plot=True):
-    '''Generates scatter plot and trend line or the r squared, coefficient and number of countries (plot=False) in comparison between two indexes (list of strs) in a given year (int)'''
+def trend(data, index, countries, years):
+    '''
+    Generates a trend graph for a given index in a given period of time for any set of countries
     
-    if type(year) == int:
-        df = np.array(analyze(data, year)[indexes].dropna())
-    else:
-        df = np.array(analyze(data, year[0])[[indexes[0]]].join(analyze(data, year[1])[indexes[1]]).dropna())
+    Parameters:
+    -----------
     
-    if df.shape[0] < 20:
-        raise ValueError('Without enough data')
+        data -> dataframe of data
+        index -> index string
+        countries -> list with indexes strings
+        years -> list with integers indicating the start and end year for the analysis
         
-    x = df[:,0].reshape(-1, 1)
-    y = df[:,1].reshape(-1, 1)
-    xlist = [(np.log(x), "log(x)"), (x, "x")] if x.min() > 0 else [(x, "x")]
-    ylist = [(np.log(y), "log(y)"), (y, "y")] if y.min() > 0 else [(y, "y")]
-    r2 = 0
-    for i in xlist:
-        for k in ylist:
-            xin, yin = remove_outliers(i[0], k[0])
-            modeltest = linear_model.LinearRegression().fit(xin, yin)
-            r2test = modeltest.score(xin, yin)
-            if r2test > r2:
-                x, y = xin, yin
-                leix, leiy = i[1], k[1]
-                model, r2 = modeltest, r2test
-                    
-    if plot:
-        plt.plot(x, model.predict(x), color='red', linewidth=2.)
-        plt.scatter(x, y, color='blue', s=50, alpha=.5)
-        plt.legend(["R**2 = {:0.4}".format(r2)])
-        plt.title('Tendency line {} - {}'.format(leix, leiy))
-        plt.xlabel(indexes[0])
-        plt.ylabel(indexes[1])
+    Return:
+    -------
+        
+        matplotlib.pyplot.plot with the trend graph
+    
+    Example:
+    --------
+    
+        trend(data, 'GDP per capita (current US$)', ['Afghanistan', 'Brazil'], [2000, 2017]) 
+    '''
+    
+    fig = plt.figure(figsize=(10, 5))
+    
+    for country in countries:
+        plt.plot(data.loc[(index, country), [str(year) for year in range(years[0], years[1] + 1)]])
+    
+    plt.title('Tendency')
+    plt.legend(countries)
+    plt.xlabel('Years')
+    plt.ylabel(index)
+    plt.setp(plt.xticks()[1], rotation=60)
+    plt.show()
+    
+def xsml(data, indexes, years, X):
+    '''
+    Generates a bar graph with the X smallest, the X medium, and the X largest values of an index or two charts with comparison
+between two indices in their respective years
+    
+    Parameters:
+    -----------
+    
+        data -> dataframe of data
+        indexes -> index string or list with indexes strings
+        years -> integer or list with integers indicating the year(s) for analysis
+        X -> integer indicating the number of countries for the analyze 
+        
+    Return:
+    -------
+        
+        matplotlib.pyplot.plot with the bar graph(s)
+        
+    Examples:
+    ---------
+    
+        xsml(data,  'Economic freedom', 2017, 3)
+        
+        xsml(data,  ['Economic freedom', 'GDP per capita (current US$)'], 2017, 3)
+    '''
+    
+    if type(indexes) == str:
+        df = data.loc[indexes].drop('World')[str(years)].dropna().sort_values()
+        fig = plt.figure(figsize=(10, 5))
+        
+        amount = int(df.shape[0]/2)-int(X/2)
+        df.iloc[list(range(0,X))+list(range(amount, amount+X))+list(range(-X,0))].plot(kind="bar")
+        plt.title('Bar graph')
+        plt.xlabel('Country')
+        plt.ylabel(indexes)
         plt.show()
         
     else:
-        return r2, model.coef_[0][0], x.shape[0]
+        df1 = data.loc[indexes[0]].drop('World')[str(years[0])]
+        df2 = data.loc[indexes[1]].drop('World')[str(years[1])]
+        df = pd.DataFrame(df1).rename(columns=lambda x: "0").join(df2).dropna()
+        df['0'] = norm(df['0'])
+        df[str(years[1])] = norm(df[str(years[1])])
+        amount = int(df.shape[0]/2)-int(X/2)
+        bar1 = df.sort_values('0').iloc[list(range(0,X))+list(range(amount, amount+X))+list(range(-X,0))]
+        bar2 = df.sort_values(str(years[1])).iloc[list(range(0,X))+list(range(amount, amount+X))+list(range(-X,0))]
+        
+        fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(15, 5))
+        bar1.plot(kind='bar', ax=axes[0])
+        bar2.plot(kind='bar', ax=axes[1])
+        axes[0].set(xlabel = 'Country', ylabel = 'Normalized values',
+                    title = 'xsml {} \n'.format(indexes[0]))
+        axes[1].set(xlabel = 'Country', ylabel = 'Normalized values',
+                    title = 'xsml {} \n'.format(indexes[1]))
+        axes[0].legend(indexes)
+        axes[1].legend(indexes)
+        plt.show()
     
-def trend(data, country, index, years):
-    '''Generates the trend over the years (list of ints) of the countries (list of strs) and requested index (str)'''
+def trend_years(data, indexes, country, years, plot=True):
+    '''
+    Generates a trend graph for a given country and time period of any set of indexes or an index with its trend line. Or a tuple with the details of the trend line
     
-    for i in country:
-        plt.plot(analyze(data, "c-"+i)[index].loc[[str(i) for i in range(years[0], years[1]+1)]])
-    plt.legend(country)
-    plt.title('Tendency')
-    plt.setp(plt.xticks()[1], rotation=60)
-    plt.xlabel('Years')
-    plt.ylabel(index)
-    plt.show()
+    Parameters:
+    -----------
     
-def lmh(data, year, index, x):
-    '''Generates the x (int) smaller, middle and higher values for the indicated year (int) and index (str)'''
+        data -> dataframe of data
+        indexes ->  indice string or list with indexes strings
+        country -> country string
+        years -> list with integers indicating the start and end year for the analysis
+        plot -> boolean indicating whether the return will be a graph (True) or a tuple with the details of the trend line (False and indexes as an index string)
+        
+    Return:
+    -------
+        
+        matplotlib.pyplot.plot with the trend graph or a tuple with R-Square, trend line coefficient and the number of countries in the analysis
+        
+    Examples:
+    ---------
     
-    df = analyze(data, year).drop("World", 0)[index].dropna().sort_values()
-    amount = int(len(df)/2)-int(x/2)
-    df.iloc[list(range(0,x))+list(range(amount, amount+x))+list(range(-x,0))].plot(kind="bar")
-    plt.title('Bar plot')
-    plt.xlabel('Country')
-    plt.ylabel(index)
-    plt.show()
+        trend_years(data, ['Economic freedom', 'GDP per capita (current US$)'], 'World', [2000, 2017])
+        
+        trend_years(data, 'Economic freedom', 'World', [2000, 2017])
+        
+        trend_years(data, 'Economic freedom', 'World', [2000, 2017], False)
+    '''
     
-def trend_years(data, country, indexes, years, plot=True):
-    '''Generates the trend line or the r squared, coefficient and number of countries (plot=False) of the years (list of ints) of the country (str) and the requested index (str)'''
+    df = data.loc[(indexes, country), [str(year) for year in range(years[0], years[1] + 1)]]
+    fig = plt.figure(figsize=(10, 5))
     
     if type(indexes) == str:
-        df = analyze(data, "c-"+country)[indexes].loc[[str(i) for i in range(years[0], years[1]+1)]].dropna()
-        x = np.array(df.index, dtype="float64").reshape(-1, 1)
-        y = preprocessing.MinMaxScaler().fit_transform(np.array(df.values, dtype="float64").reshape(-1, 1))
+        df = df.dropna()
+        x = norm(df.index)
+        y = norm(df.values)
         model = linear_model.LinearRegression().fit(x, y)
+        
         if plot:
-            plt.plot(df.index, model.predict(x), color='red', linewidth=2.)
-            plt.plot(df.index, y)
-            plt.legend(["R**2 = {:0.4}".format(model.score(x, y)), "Coef = {:0.4}".format(model.coef_[0][0])])
-            plt.title('Tendency line')
+            plt.plot(x, model.predict(x), color='red', linewidth=2.)
+            plt.plot(x, y)
+            plt.title('Trend graph and trend line')
+            plt.legend(["R2 = {:0.4}".format(model.score(x, y)), "Coef = {:0.4}".format(model.coef_[0][0])])
             plt.setp(plt.xticks()[1], rotation=60)
-            plt.xlabel('Years')
-            plt.ylabel(indexes)
+            plt.xlabel('Normalized years')
+            plt.ylabel('Normalized values')
             plt.show()
 
         else:
-            return model.score(x, y), model.coef_[0][0], len(x)
+            return model.score(x, y), model.coef_[0][0], x.shape[0]
+        
     else:
-        df = analyze(data, "c-"+country)[indexes].loc[[str(i) for i in range(years[0], years[1]+1)]].dropna()
+        df = df.dropna(1)
         for ind in indexes:
-            plt.plot(df.index, preprocessing.MinMaxScaler().fit_transform(np.array(df[ind], dtype="float64").reshape(-1, 1)))
+            plt.plot(df.columns, norm(df.loc[ind]))
+            
+        plt.title('Trend graphs')
         plt.legend(indexes)
         plt.setp(plt.xticks()[1], rotation=60)
+        plt.xlabel('Years')
+        plt.ylabel('Normalized values')
         plt.show() 
-        
-def comparison_filter(data, year=2014, years=[2000,2014], country="World", index='GDP per capita (current US$)', r2=0.45, coef=0.05, num=10):
-    '''Generates index graphs that match the index and criteria provided'''
-    
-    indcoef = trend_years(data, country, index, years, False)[1]
-    for ind in analyze(data, year).columns.drop(index):
-        try:
-            isc = scatter(data, year, [index]+[ind], False)
-            itr = trend_years(data, country, ind, years, False)[1]
-            if isc[0] >= r2 and abs(itr-indcoef) <= coef and isc[2] >= num:
-                print(index + " and " + ind)
-                scatter(data, year, [index]+[ind])
-                trend_years(data, country, [index]+[ind], years)
-        except:
-            pass
